c781db8af31f52bf8f8edbb28d44d906bf12e88d
terrok9 <jf.barrera10@uniandes.edu.co>
2023-06-29 08:43:18 -0500

    packages

--- setup.py
+++ setup.py
@@ -13,8 +13,19 @@ setup(
     url='https://github.com/lgomezt/Tidytweets',
     keywords='NLP Text processing Twitter API processing data cleaning',
     install_requires = [],
+     classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires = ">=3.6"
     setup_requires = ['pytest-runner'],
     tests_require = ['pytest'],
+    install_requires = [
+        'pandas','numpy','nltk','spacy','gensim','textblob','tweepy',
+        'emoji','unidecode','scikit-learn','matplotlib','seaborn','wordcloud',
+        'plotly','pyLDAvis','thefuzz'
+    ],
     # Running `python setup.py pytest`
     # will execute all tests stored in the "tests" folder.
     test_suite = 'tests'
