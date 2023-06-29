42cd9e4811f00ec6874e92a69f954fd8cbc8e281
terrok9 <jf.barrera10@uniandes.edu.co>
2023-06-29 09:05:35 -0500

    uploaded version 0.1.0 to pypi

--- setup.py
+++ setup.py
@@ -2,18 +2,16 @@ from setuptools import find_packages, setup
 
 setup(
     name = 'tidytweets',
-    packages = find_packages(include = ['tidytweets']),
+    packages = find_packages('tidytweets',include = ['tidytweets']),
     version = '0.1',
     description = 'Clean tweets to perform various NLP tasks such as topic analysis, word embeddings, sentiment analysis, etc.',
     author = 'Lucas Gómez Tobón, Jose Fernando Barrera',
     author_email  = "lucasgomeztobon@hotmail.com, jf.barrera10@uniandes.edu.co",
     license = 'MIT',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
+    package_dir={'': 'tidytweets'},
     url='https://github.com/lgomezt/Tidytweets',
     keywords='NLP Text processing Twitter API processing data cleaning',
-    install_requires = [],
-     classifiers = [
+    classifiers = [
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
