67841313907c5057da0f96c1b73677101589105e
Lucas Antonio II <lucasgomeztobon@gmail.com>
2023-09-03 23:23:05 -0500

    cambios

---
+++ setup.py
@@ -0,0 +1,40 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+# The directory containing this file
+HERE = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(HERE, 'README.md'), encoding = 'utf-8') as f:
+    long_description = f.read()
+
+# This call to setup() does all the work
+setup(
+    name = "tidyX",
+    version = "0.1.0",
+    description = "Python package to clean raw tweets for ML applications",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "",
+    author = "Lucas Gómez Tobón",
+    author_email = "lucasgomeztobon@gmail.com",
+    license = "MIT",
+    classifiers = [
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent"
+    ],
+    packages = ["TidyX"],
+    include_package_data = True,
+    install_requires = ["numpy"]
+)
