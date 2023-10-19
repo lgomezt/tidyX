84e8e72f5f5c51e429ede6955af51df2386b2c2c
terrok9 <jf.barrera10@uniandes.edu.co>
2023-10-19 16:25:35 -0500

    modified

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from tidyX import text_preprocessor
+from tidyX import TextPreprocessor
 
-text_preprocessor.preprocess("Hola! ¿Cómo estás?")
 string_example = "Holaaaaa! Cómo vasss?"
 string_without_repetitions = TextPreprocessor.TextPreprocessor.remove_repetitions(string=string_example, exceptions=None)
 print("After:", string_without_repetitions)
